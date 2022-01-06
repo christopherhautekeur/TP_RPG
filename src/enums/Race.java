407dcc943cb1808c520147a63c2c619ac7b1e414
Christopher Hautekeur <christopher.hautekeur@gmail.com>
2022-01-06 15:54:36 +0100

    Initial commit

---
+++ src/enums/Race.java
@@ -0,0 +1,3 @@
+package enums;
+
+public enum Race { HUMAIN, NAIN, ELF }
---
+++ src/models/Personnage.java
@@ -0,0 +1,10 @@
+package models;
+
+public abstract class Personnage {
+    private String nom;
+    private int niveau, experience, vieMax, vieAct, manaMax, manaAct, force, intelligence, defense, defenseMagique;
+    private Race race;
+
+    public Personnage(){}
+
+}
